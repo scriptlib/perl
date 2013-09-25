40eaafdef8ab9d87d045c1d7fe1bc7df834939bf
Eotect Nahn <eotect@gmail.com>
2013-09-26 06:18:49 +0800

    Add/Update 'opml2txt.pl'

---
+++ opml2txt.pl
@@ -0,0 +1,78 @@
+#!/usr/bin/perl -w
+# $Id$
+package MyPlace::Script::opml2txt;
+use strict;
+
+our $VERSION = 'v0.1';
+my @OPTIONS = qw/
+	help|h|? 
+	manual|man
+/;
+my %OPTS;
+if(@ARGV)
+{
+    require Getopt::Long;
+    Getopt::Long::GetOptions(\%OPTS,@OPTIONS);
+}
+else {
+	$OPTS{'help'} = 1;
+}
+if($OPTS{'help'} or $OPTS{'manual'}) {
+	require Pod::Usage;
+	my $v = $OPTS{'help'} ? 1 : 2;
+	Pod::Usage::pod2usage(-exitval=>$v,-verbose=>$v);
+    exit $v;
+}
+
+
+__END__
+
+=pod
+
+=head1  NAME
+
+opml2txt - PERL script
+
+=head1  SYNOPSIS
+
+opml2txt [options] ...
+
+=head1  OPTIONS
+
+=over 12
+
+=item B<--version>
+
+Print version infomation.
+
+=item B<-h>,B<--help>
+
+Print a brief help message and exits.
+
+=item B<--manual>,B<--man>
+
+View application manual
+
+=item B<--edit-me>
+
+Invoke 'editor' against the source
+
+=back
+
+=head1  DESCRIPTION
+
+___DESC___
+
+=head1  CHANGELOG
+
+    2013-09-01 21:47  xiaoranzzz  <xiaoranzzz@Mainstream>
+        
+        * file created.
+
+=head1  AUTHOR
+
+xiaoranzzz <xiaoranzzz@Mainstream>
+
+=cut
+
+#       vim:filetype=perl
