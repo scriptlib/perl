b6e70861c997f8b68452fbee94d34e6b91bcabba
Eotect Nahn <eotect@gmail.com>
2014-11-06 00:04:32 +0800

    Update file: search_torrents.pl

--- search_torrents.pl
+++ search_torrents.pl
@@ -2,12 +2,14 @@
 # $Id$
 package MyPlace::Script::search_torrents;
 use strict;
+use MyPlace::Script::Message;
 
 our $VERSION = 'v0.1';
 my @OPTIONS = qw/
 	help|h|? 
 	manual|man
 	download|d
+	engine|e:s
 /;
 my %OPTS;
 if(@ARGV)
@@ -25,14 +27,54 @@ if($OPTS{'help'} or $OPTS{'manual'}) {
     exit $v;
 }
 
-my @app = ('urlrule_action',"http://torrentproject.se/?t=" . join(" ",@ARGV),1);
-if($OPTS{download}) {
-	exec @app,"download_torrent";
+
+my %ENGINES = (
+	'torrentproject'=>[
+		'http://torrentproject.se/?safe=off&t=QUERY',
+		2,
+	],
+	'bitsnoop'=>[
+		'http://bitsnoop.com/search/all/QUERY+safe:no/c/d/1',
+		2,
+	],
+	'torrentkitty.org'=>[
+		'http://www.torrentkitty.org/search/QUERY/',
+		1,
+	],
+);
+
+
+
+my %engine;
+my @DATA;
+
+if($OPTS{engine}) {
+	foreach(keys %ENGINES) {
+		if(m/$OPTS{engine}/) {
+			$engine{$_} = $ENGINES{$_};
+		}
+	}
 }
 else {
-	exec @app;
+	%engine = %ENGINES;
 }
 
+if(!%engine) {
+	app_error "No search engine specified\n";
+	exit 1;
+}
+
+foreach my $QUERY(@ARGV) {
+	foreach(keys %engine) {
+		app_message2 "Search \"$QUERY\" using engine [$_]\n";
+		my $url = $engine{$_}[0];
+		my $level = $engine{$_}[1];
+		$url =~ s/QUERY/$QUERY/g;
+		my @prog = ('urlrule_action',$url,$level);
+		push @prog,"SAVE" if($OPTS{download});
+		system(@prog);
+	}
+}
 
 __END__
 
