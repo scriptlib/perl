6f3161352dde6057053d8915b9c53c26566dfa66
Eotect Nahn <eotect@gmail.com>
2015-05-04 22:23:38 +0800

    Move urlrule.pl, urlrule_query to project [urlrule]

--- urlrule_query.pl
+++ urlrule_query.pl
@@ -1,12 +1,1 @@
-#!/usr/bin/perl -w
-# $Id$
-use strict;
-use MyPlace::Program::SimpleQuery;
-
-
-my $q = MyPlace::Program::SimpleQuery->new('--list');
-exit $q->execute(@ARGV);
-
-__END__
-
-#       vim:filetype=perl
+../urlrule/bin/urlrule_query.pl
