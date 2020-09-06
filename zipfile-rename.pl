fe17b7ddf8ac231768f01ba498930b05c76c3a7e
Eotect Nahn <eotect@gmail.com>
2020-09-07 02:20:34 +0800

    [GIT-ACOMMIT] Update 9 files:
    	escape
    	js_escape
    	modules
    	mycurl
    	urlrule
    	urlrule_get
    	urlrule_new
    	urlrule_search
    	zipfile-rename.pl

---
+++ zipfile-rename.pl
@@ -0,0 +1,24 @@
+﻿#!/usr/bin/perl
+use strict;
+open FI,'<','Encoding Errors.txt' or die("$!\n");
+foreach(<FI>) {
+	chomp;
+	s/[\r\n]+$//;
+	if(m/^(.+)\s+-\>\s+(.+)$/) {
+		my $new = $1;
+		my $old = $2;
+		if(-f $old) {
+			if($new =~ m/\//) {
+				my $pd = $new;
+				$pd =~ s/\/[^\/]+$//;
+				if(! -d $pd) {
+					system("mkdir","-p","-v",$pd);
+				}
+			}
+			print STDERR "Rename $old => $new ...\n";
+			rename $old,$new;
+		}
+	}
+}
+close FI;
+
